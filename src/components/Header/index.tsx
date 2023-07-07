50a956cf0fd8bcf22bc597468ca9b523a57d48fb
mplara7 <mpslara11@gmail.com>
2023-07-07 15:18:57 -0300

    creating-header-pt1

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import  styles from './styles.module.scss'
+import Image from 'next/image'
+import logo from '../../../public/images/logo.svg'
+
+import Link from 'next/link'
+
+export function Header(){
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a>
+          <Image src={logo} alt='Sujeito Progamador Logo'/>
+        </a>
+
+        <nav>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+          <Link href='/posts'>
+            <a>Conteúdos</a>
+          </Link>
+          <Link href='/sobre'>
+            <a>Quem somos?</a>
+          </Link>
+        </nav>
+
+        <a className={styles.readyButton} type='button' href='https://sujeitoprogramdor.com'> Começar</a>
+
+      </div>
+    </header>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from 'next/app'
-
 import '../styles/global.css'
 
+import { Header } from '../components/Header'
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Header/>
+      <Component {...pageProps} />
+    </>
+  )
 }
