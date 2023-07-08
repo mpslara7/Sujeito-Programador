159d228998f8f31354a53c362786da149200ab9c
mplara7 <mpslara11@gmail.com>
2023-07-07 21:34:30 -0300

    creation-component-active-link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,26 +2,30 @@ import  styles from './styles.module.scss'
 import Image from 'next/image'
 import logo from '../../../public/images/logo.svg'
 
-import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink'
 
 export function Header(){
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <a>
-          <Image src={logo} alt='Sujeito Progamador Logo'/>
-        </a>
+        <ActiveLink href='/' activeClassName={styles.active}>
+          <a>
+            <Image src={logo} alt='Sujeito Progamador Logo'/>
+          </a>
+        </ActiveLink>
 
         <nav>
-          <Link href='/'>
+          <ActiveLink href='/' activeClassName={styles.active}>
             <a>Home</a>
-          </Link>
-          <Link href='/posts'>
+          </ActiveLink>
+
+          <ActiveLink href='/posts' activeClassName={styles.active}>
             <a>Conteúdos</a>
-          </Link>
-          <Link href='/sobre'>
+          </ActiveLink>
+
+          <ActiveLink href='/sobre' activeClassName={styles.active}>
             <a>Quem somos?</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <a className={styles.readyButton} type='button' href='https://sujeitoprogramdor.com'> Começar</a>
