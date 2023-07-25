415c4b062f6b14725e0183411d8772627efc8112
mplara7 <mpslara11@gmail.com>
2023-07-25 11:39:47 -0300

    stylization-first-structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,23 @@ export default function Home() {
       <Head>
         <title>Sujeito Programador</title>
       </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando voçê para o proximo nivel!!</h1>
+            <span>Uma plataforma que te leva para o proximo nivel</span>
+            <a>
+              <button>
+                COMEÇAR AGORA !
+              </button>
+            </a>
+          </section>
+
+          <img src='/images/banner-conteudos.png'
+                 alt='Conteudos Sujeito Programador'
+          />
+        </div>
+      </main>
     </>
 )
 };
